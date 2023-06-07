1957650593493bc11877a677a9bd49ce9f25a558
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-08 00:10:12 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
+
   has_one_attached :image
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category_id
+  belongs_to_active_hash :condition_id
+  belongs_to_active_hash :delivery_charge_id
+  belongs_to_active_hash :prefecture_id
+  belongs_to_active_hash :handling_time_id
+
+
+  validates :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A\d+\z/ }
+end
+
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, :numericality: { other_than: 0 }
+
 end
