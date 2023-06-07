069efda662d1c4fa5ec1ba9eee6a13c69924112c
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-07 19:35:22 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -64,5 +64,7 @@ group :production do
   gem 'pg'
 end
 
-
 gem 'devise'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, :user).merge(user_id: current_user.id)
+  end
 end
