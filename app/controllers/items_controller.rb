9be39c10040bbf5a06c880c9613db43ad6b4ae0b
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-08 22:41:31 +0900

    出品が完了したら、トップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,19 @@ class ItemsController < ApplicationController
 
 
   def create
-
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end  
 
   private
 
+
   def item_params
-    params.require(:item).permit(:image, :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, :user).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
   end
+
 end
