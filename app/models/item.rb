06fd94eb42ec1930c076eddb0d03faf44e054a8a
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-09 20:50:09 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :handling_time
 
 
-  validates :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, presence: true
+  validates :image, :name, :detail, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A\d+\z/ }
 
 
-  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, numericality: { other_than: 0 }
-
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :handling_time_id, inclusion: { in: 1.., message: "Select" }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/test_image.png')
+  end
+
+  describe '商品出品' do
+    context '出品がうまくいくとき' do
+      it ':image,:name,:detail,:price,:category_id,f:condition_id,delivery_charge_id,:perfecture_id,:handling_time_idが存在すれば登録できる' do
+        expect(@item.valid?).to eq true
+      end
+      it ':priceが半角数字でかつ300〜9999999の間の金額であるとき' do
+        @item.price = 9_999_999
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品がうまくいかないとき' do
+      it ':imageが空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it ':nameが空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it ':detailが空だと登録できない' do
+        @item.detail = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+      it ':priceが空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it ':priceが半角数字以外だと登録できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it ':priceが300~9999999の範囲外だと登録できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it ':category_idが「---」だと登録できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+      it ':condition_idが「---」だと登録できない' do
+        @item.condition_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition Select')
+      end
+      it ':delivery_charge_idが「---」だと登録できない' do
+        @item.delivery_charge_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery charge Select')
+      end
+      it ':prefecture_idが「---」だと登録できない' do
+        @item.prefecture_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture Select')
+      end
+      it ':handling_time_idが「---」だと登録できない' do
+        @item.handling_time_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Handling time Select')
+      end
+    end
+  end
 end
