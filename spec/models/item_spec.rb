57f34233c1c83ebe4baa6a9000a1dc57e81fed8e
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-12 16:43:55 +0900

    修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -79,9 +79,9 @@ RSpec.describe Item, type: :model do
       end
 
       it 'ユーザー情報がない場合は登録できない' do
-        item = Item.new
-        expect(item).not_to be_valid
-        expect(item.errors[:user]).to include('must exist')
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
