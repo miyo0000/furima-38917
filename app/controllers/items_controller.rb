7124f3c0a0cc996909a4c8f63ba092b71763ab26
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-18 15:25:50 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update]
-  before_action :set_item, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -24,15 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
 
     if @item.update(item_params)
       redirect_to item_path(@item)
