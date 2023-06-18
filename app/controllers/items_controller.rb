242c8028f4f95079a4dba7af20ed3e9fabc58dcb
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-18 22:30:48 +0900

    authenticate_user!にdestroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
 
