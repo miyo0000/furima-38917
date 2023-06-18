ac090b7050086d479edc07d2c5acf688158fd502
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-06-18 15:21:35 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :update]
   before_action :set_item, only: [:edit]
   before_action :move_to_index, only: [:edit]
 
@@ -53,7 +53,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    authenticate_user!
     redirect_to root_path unless @item && current_user.id == @item.user_id
   end
 end
