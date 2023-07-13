90dc71fde29eae7d9d6273c727d904cec900889e
miyo0000 <miyopan.rina.maru1928@gmail.com>
2023-07-13 15:40:49 +0900

    使用するブランチを「main」に変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.17.3'
 set :application, 'furima-38917'
 
 set :repo_url,  'git@github.com:miyo0000/furima-38917.git'
-
+set :branch, 'main'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
